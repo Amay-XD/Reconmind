66583ed22c8ab05f901802f1d67fe1c29a262dc6
Amay-XD <amayjogdand17@gmail.com>
2026-05-26 14:12:11 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+SHODAN_API_KEY    = os.environ.get("SHODAN_API_KEY", "kLPaIlm1Qr6mTp3gZR4UG2GjAdOnp7oD")
+ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", "")
+GITHUB_TOKEN      = os.environ.get("GITHUB_TOKEN", "")
