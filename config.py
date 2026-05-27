2675f8de65a18ee43cfa006706993af1e9409822
Amay-XD <amayjogdand17@gmail.com>
2026-05-27 12:42:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-SHODAN_API_KEY    = os.environ.get("SHODAN_API_KEY", "kLPaIlm1Qr6mTp3gZR4UG2GjAdOnp7oD")
-ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", "")
+SHODAN_API_KEY    = os.environ.get("SHODAN_API_KEY", "")
+GROQ_API_KEY      = os.environ.get("GROQ_API_KEY", "")
 GITHUB_TOKEN      = os.environ.get("GITHUB_TOKEN", "")
